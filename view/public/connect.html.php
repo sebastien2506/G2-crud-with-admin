edae14d8f49a3739abbb6347d111917dc15df3d7
sebastien2506 <jestrovic_8@msn.com>
2024-04-18 14:36:14 +0200

    exe1

--- view/public/connect.html.php
+++ view/public/connect.html.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Connexion</title>
 </head>
 <body>
@@ -18,8 +19,22 @@
         <input type="password" name="passwd" placeholder="Votre mot de passe" required><br>
         <input type="submit" value="connexion">
     </form>
+    
+    <form>
+  <div class="form-group" action="" method="POST" name="connexion">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" name="username" aria-describedby="emailHelp" placeholder="Enter email" required>
+    <small id="emailHelp" class="form-text text-muted"></small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" name="passwd" placeholder="Password" required>
+  </div><br>
+  <button type="submit" value="connexion" class="btn btn-primary">Submit</button>
+</form>
     <?php
     // var_dump($_POST);
     ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
